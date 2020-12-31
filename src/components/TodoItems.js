6310a9ba825ae61619a40934d75c4ff3f4dd3b94
Cemil Okay <cemilokay@gmail.com>
2020-12-31 04:18:22 +0000

    Add tasks to the state

---
+++ src/components/TodoItems.js
@@ -0,0 +1,12 @@
+function TodoItems (props) {
+  return props.tasks.map(item => {
+    return (
+      <li>
+        {item.title} &nbsp;
+        <span className="remove-item">(Remove)</span>
+      </li>
+    )
+  });
+}
+
+export default TodoItems;
